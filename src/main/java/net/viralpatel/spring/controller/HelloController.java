b5a521b4a025ceac372ad6812896807d7dfc8bc2
selandry <serge.landry@gmail.com>
2017-10-17 14:33:35 -0400

    add hello to output

--- src/main/java/net/viralpatel/spring/controller/HelloController.java
+++ src/main/java/net/viralpatel/spring/controller/HelloController.java
@@ -10,7 +10,7 @@ public class HelloController {
 	@GetMapping("/")
 	public String hello(Model model) {
 
-		model.addAttribute("name", "John Doe");
+		model.addAttribute("name", "hello feature!!");
 
 		return "welcome";
 	}
